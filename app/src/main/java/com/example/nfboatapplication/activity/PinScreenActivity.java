64a6ff86d375390bb96a7acb793ff5805062ba87
MuhammadSaif <safisardar248@gmail.com>
2022-05-26 23:23:59 +0500

    init NFBoat

---
+++ app/src/main/java/com/example/nfboatapplication/activity/PinScreenActivity.java
@@ -0,0 +1,17 @@
+package com.example.nfboatapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nfboatapplication.R;
+
+public class PinScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin_screen);
+
+    }
+}
