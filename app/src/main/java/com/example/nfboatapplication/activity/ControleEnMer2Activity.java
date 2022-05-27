ffe43921d864340e01bf77f02840afe1903a3305
MuhammadSaif <safisardar248@gmail.com>
2022-05-28 00:10:32 +0500

    init NFBoat

---
+++ app/src/main/java/com/example/nfboatapplication/activity/ControleEnMer2Activity.java
@@ -0,0 +1,16 @@
+package com.example.nfboatapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nfboatapplication.R;
+
+public class ControleEnMer2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controle_en_mer2);
+    }
+}
---
+++ app/src/main/java/com/example/nfboatapplication/activity/WalletSearchActivity.java
@@ -0,0 +1,16 @@
+package com.example.nfboatapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nfboatapplication.R;
+
+public class WalletSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallet_search);
+    }
+}
