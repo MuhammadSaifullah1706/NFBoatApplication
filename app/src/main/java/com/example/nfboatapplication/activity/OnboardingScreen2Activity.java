ca26974f62002ae7b977cfeddc4fb7b142cf0a5a
MuhammadSaif <safisardar248@gmail.com>
2022-06-02 00:30:49 +0500

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	.idea/misc.xml

---
+++ app/src/main/java/com/example/nfboatapplication/activity/OnboardingScreen2Activity.java
@@ -0,0 +1,16 @@
+package com.example.nfboatapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nfboatapplication.R;
+
+public class OnboardingScreen2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding_screen2);
+    }
+}
