3a6581cae2ac966233761111ab754c7e7febc54e
MuhammadSaif <safisardar248@gmail.com>
2022-06-24 18:34:31 +0500

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	.idea/misc.xml

---
+++ app/src/main/java/com/example/nfboatapplication/workflow6/Authentification1facteursActivity.java
@@ -0,0 +1,16 @@
+package com.example.nfboatapplication.workflow6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nfboatapplication.R;
+
+public class Authentification1facteursActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentification1facteurs);
+    }
+}
---
+++ app/src/main/java/com/example/nfboatapplication/workflow6/ProfilStep4Activity.java
@@ -0,0 +1,16 @@
+package com.example.nfboatapplication.workflow6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nfboatapplication.R;
+
+public class ProfilStep4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_step4);
+    }
+}
