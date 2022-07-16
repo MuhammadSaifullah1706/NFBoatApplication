e9b59029e943a0cd902dd9a9b683b0f49d16221e
MuhammadSaif <safisardar248@gmail.com>
2022-07-17 00:44:46 +0500

    init

--- app/src/main/java/com/example/nfboatapplication/bottomnavigation/MainActivity3.kt
+++ app/src/main/java/com/example/nfboatapplication/bottomnavigation/MainActivity3.kt
@@ -2,21 +2,20 @@ package com.example.nfboatapplication.bottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.ui.setupWithNavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.nfboatapplication.R
-import com.example.nfboatapplication.databinding.ActivityMainBinding
+import com.example.nfboatapplication.databinding.ActivityMain3Binding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity3 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain3Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMain3Binding.inflate(layoutInflater)
         setContentView(binding.root)
         //Navigation Controller Settings
         val navController = findNavController(R.id.nav_fragment)
         binding.bottomNavigationView.setupWithNavController(navController)
-
     }
 }
